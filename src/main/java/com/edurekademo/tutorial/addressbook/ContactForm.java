7a77f791f84779202b14bad5db23cb2aa6ea6372
snagalla14 <161551360+snagalla14@users.noreply.github.com>
2024-03-09 13:13:37 +0530

    Update ContactForm.java
    phone to mobile

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -27,7 +27,7 @@ public class ContactForm extends FormLayout {
     Button cancel = new Button("Cancel", this::cancel);
     TextField firstName = new TextField("First name");
     TextField lastName = new TextField("Last name");
-    TextField mobile = new TextField("Phone");
+    TextField mobile = new TextField("mobile");
     TextField email = new TextField("Email");
     DateField birthDate = new DateField("Birth date");
 
@@ -60,7 +60,7 @@ public class ContactForm extends FormLayout {
         HorizontalLayout actions = new HorizontalLayout(save, cancel);
         actions.setSpacing(true);
 
-        addComponents(actions, firstName, lastName, phone, email, birthDate);
+        addComponents(actions, firstName, lastName, mobile, email, birthDate);
     }
 
     /*
